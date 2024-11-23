e1b526dea99efa986182e5da5e5b6fe0411d3095
tryllati <tryllati@gmail.com>
2024-11-23 13:06:47 +0100

    Some methods added to SessionServiceInterface

--- app/Services/SessionServiceInterface.php
+++ app/Services/SessionServiceInterface.php
@@ -4,10 +4,20 @@ namespace App\Services;
 
 use App\Models\Session;
 use App\Models\SessionTestVariant;
+use App\Models\Test;
+use App\Models\TestVariant;
 
 interface SessionServiceInterface
 {
     public function session(): Session;
 
     public function sessionTestVariants(): SessionTestVariant;
+
+    public function sessionContainTestVariantByTestId(int $testId): bool;
+
+    public function createDbSessionTestVariant(TestVariant $variant): void;
+
+    public function createSessionTestEvent(Test $test, TestVariant $variant): void;
+
+    public function saveTestVariantInSession(TestVariant $variant): void;
 }
